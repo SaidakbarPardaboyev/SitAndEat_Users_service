2d0ce2ac53807842508bf61cc6d6a992c5dbb3ac
pronium <azizbekqobulov05@gmail.com>
2024-07-08 16:35:45 +0500

    testing done

--- Storage/postgres/Users.go
+++ Storage/postgres/Users.go
@@ -14,7 +14,13 @@ func NewUsers(db *sql.DB) *Users {
 }
 
 func (u *Users) GetProfile(user *pb.UserId) (*pb.GetUser, error) {
-	query := "Select user_name, email, phone_number from users where id = $1"
+	query := 
+	`Select 
+		user_name, email, phone_number 
+	from 
+		users 
+	where 
+		id = $1`
 
 	row := u.db.QueryRow(query, user.UserId)
 	var name, email, phone string
