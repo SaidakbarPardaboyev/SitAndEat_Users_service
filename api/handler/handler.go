bdfb4cb413afd3aae8db6444fec1a64a314cb2b2
Saidakbar_Pardaboyev <pardaboyevsaidakbar103@gmail.com>
2024-07-10 15:41:41 +0500

    some changes

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -2,13 +2,20 @@ package handler
 
 import (
 	"database/sql"
+	"log/slog"
 	"restaurant/Storage/postgres"
+	"restaurant/pkg/logger"
 )
 
 type Handler struct {
+	logger   slog.Logger
 	UserRepo *postgres.UserRepo
 }
 
 func NewHandler(db *sql.DB) *Handler {
-	return &Handler{postgres.NewUserRepo(db)}
+	logger := logger.NewLogger()
+	return &Handler{
+		UserRepo: postgres.NewUserRepo(db),
+		logger:   logger,
+	}
 }
